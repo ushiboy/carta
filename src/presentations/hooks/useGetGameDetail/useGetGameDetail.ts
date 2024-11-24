3dbb088817534573888b6f22c00a91cf253e68f9
ushiboy <ushiboy.dev@gmail.com>
2024-11-24 21:21:25 +0900

    fix cache

--- src/presentations/hooks/useGetGameDetail/useGetGameDetail.ts
+++ src/presentations/hooks/useGetGameDetail/useGetGameDetail.ts
@@ -5,9 +5,12 @@ import { useRepository } from "@/presentations/contexts/RepositoryContext";
 export function useGetGameDetail(gameId: number) {
   const { gameRepository } = useRepository();
 
-  const { data, isLoading } = useSWR(gameRepository.getGameDetail.name, () => {
-    return gameRepository.getGameDetail(gameId);
-  });
+  const { data, isLoading } = useSWR(
+    [gameRepository.getGameDetail.name, gameId],
+    () => {
+      return gameRepository.getGameDetail(gameId);
+    },
+  );
 
   return {
     game: data,
