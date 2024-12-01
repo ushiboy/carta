7b20e80e93c9a9fa76dafc89c1a4b72e20f01253
ushiboy <ushiboy.dev@gmail.com>
2024-12-01 14:12:54 +0900

    styled import data page

--- src/presentations/pages/ImportDataPage/ImportDataPage.tsx
+++ src/presentations/pages/ImportDataPage/ImportDataPage.tsx
@@ -1,6 +1,13 @@
-import { useId } from "react";
-
-import { Loading } from "@/presentations/shared/Loading";
+import {
+  Base,
+  Button,
+  DropZone,
+  FormControl,
+  Input,
+  PageHeading,
+  Stack,
+  Text,
+} from "smarthr-ui";
 
 type Props = {
   title: string;
@@ -21,80 +28,61 @@ export function ImportDataPage({
   onFileChange,
   onImport,
 }: Props) {
-  const id = useId();
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        onImport();
-      }}
-    >
-      <div className="flex">
-        <div className="w-full rounded bg-white p-6 shadow-md">
-          <h1 className="mb-4 text-2xl font-bold">CSVから登録</h1>
-          <div className="mb-4">
-            <label
-              className="mb-2 block text-sm font-medium text-gray-700"
-              htmlFor={`${id}-title`}
-            >
-              タイトル
-            </label>
-            <input
-              type="text"
-              id={`${id}-title`}
-              data-testid="inputTitle"
-              className="w-full rounded border border-gray-300 px-4 py-2 focus:outline-none focus:ring focus:ring-blue-200"
-              placeholder="Enter a title"
-              value={title}
-              onChange={(e) => onTitleChange(e.target.value)}
-            />
-          </div>
-
-          <div className="mb-4">
-            <label
-              className="mb-2 block text-sm font-medium text-gray-700"
-              htmlFor={`${id}-file`}
-            >
-              インポートCSVファイル
-            </label>
-            <div className="flex items-center">
-              <input
-                type="file"
-                id={`${id}-file`}
+    <Stack data-testid="importDataPage">
+      <PageHeading>CSVインポート</PageHeading>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          onImport();
+        }}
+      >
+        <Base padding={1}>
+          <Stack gap={1.5}>
+            {!fileName ? (
+              <DropZone
+                name="csvfile"
                 data-testid="inputCsvFile"
-                className="hidden"
+                onSelectFiles={(_, files) => files && onFileChange(files[0])}
                 accept=".csv"
-                onChange={(e) =>
-                  e.target.files && onFileChange(e.target.files[0])
-                }
-              />
-              <label
-                htmlFor={`${id}-file`}
-                className="cursor-pointer rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
               >
-                ファイル選択
-              </label>
-              <span className="ml-4 text-gray-600">
-                {fileName || "CSVファイルを選択してください"}
-              </span>
-            </div>
-          </div>
+                <Text>
+                  <span>ここにCSVファイルをドラッグアンドドロップ</span>
+                  <span>または</span>
+                </Text>
+              </DropZone>
+            ) : (
+              <>
+                <FormControl
+                  title="タイトル"
+                  statusLabelProps={{ type: "red", children: "必須" }}
+                >
+                  <Input
+                    name="title"
+                    data-testid="inputTitle"
+                    type="text"
+                    value={title}
+                    className="h-11 w-72 md:w-96"
+                    onChange={(e) => onTitleChange(e.target.value)}
+                  />
+                </FormControl>
 
-          <div className="flex">
-            <button
-              data-testid="buttonImport"
-              disabled={!enableImport || isLoading}
-              type="submit"
-              className="rounded bg-green-500 px-4 py-2 font-medium text-white hover:bg-green-600 disabled:bg-gray-300"
-            >
-              インポート
-            </button>
-            <div className="ml-2">
-              <Loading show={isLoading} />
-            </div>
-          </div>
-        </div>
-      </div>
-    </form>
+                <div>
+                  <Button
+                    variant="primary"
+                    data-testid="buttonImport"
+                    disabled={!enableImport}
+                    type="submit"
+                    loading={isLoading}
+                  >
+                    インポート
+                  </Button>
+                </div>
+              </>
+            )}
+          </Stack>
+        </Base>
+      </form>
+    </Stack>
   );
 }
